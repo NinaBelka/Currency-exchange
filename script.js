333f131c2414369bebbe0abb546e99535670578a
Nina <mysha76@mail.ru>
2020-01-07 13:50:52 +0300

    Выполнена доработка по замечаниям

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ let exchange = document.querySelector('.exchange');
 
 let menu = document.querySelector('.menu');
 
-function buildTable() {
-    menu.addEventListener('click', function (event) {
-        let textContent = event.target.textContent;
-        btn.innerHTML = `${textContent}`;
-        exchange.innerHTML = `${textContent} Exchange rate`;
-        ratesMoneys(textContent);
-    });
+function buildTable(event) {
+    let textContent = event.target.textContent;
+    btn.innerHTML = `${textContent}`;
+    exchange.innerHTML = `${textContent} Exchange rate`;
+
+    ratesMoneys(textContent);
 };
+menu.addEventListener('click', buildTable);
 
 async function ratesMoneys(textContent) {
     let response = await fetch(`https://api.exchangerate-api.com/v4/latest/${textContent}`);
@@ -19,11 +19,10 @@ async function ratesMoneys(textContent) {
     let arrValue = Object.values(moneys.rates);
     rateTable(arrKey, arrValue);
 };
-buildTable();
 
 function rateTable(arrKey, arrValue) {
     let tbody = document.querySelector('.tbody');
-
+    tbody.innerHTML = '';
     for (let i = 0; i < arrKey.length; i++) {
         let string = `
         <tr>
